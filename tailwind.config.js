1020aaf220bc38683a5591f52c0591985ed9152f
Steve Francis King Pada <padastevefrancisking@gmail.com>
2024-10-05 00:28:44 +0800

    huhays

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        "filler": "#1b263b",
+        "header": "#0d1b2a",
+        "lightest": "#e0e1dd",
+        "cont-color1": "#415A77",
+        "cont-color2": "#778DA9",
+        "super-bg" : "#8d99ae",
+      },
+    },
+  },
+  plugins: [],
+}
+
